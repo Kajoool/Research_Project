21a06e4fc0748a207180f3b259e27e91a15e33be
Kajoool <70322066+Kajoool@users.noreply.github.com>
2021-08-16 17:41:08 -0400

    Update pixelization.m

--- pixelization.m
+++ pixelization.m
@@ -3,14 +3,14 @@ I=imread('C:\Users\Kajol\Desktop\Lung3.jpeg');
 Io=rgb2gray(I);
 figure,imshow(Io),title('Original');
 baseFileName = 'Original.jpeg'; % Whatever....
-fullFileName=fullfile('C:\Users\Shashwat\Desktop', baseFileName);
+fullFileName=fullfile('C:\Users\Kajol\Desktop', baseFileName);
 imwrite(Io,fullFileName);
 I1=imtophat(Io,strel('disk',10));
 figure,imshow(I1),title('First');
 I2=imadjust(I1);
 figure,imshow(I2),title('Second');
 histogram(I2),title('Histogram');
-  level=graythresh(I2); %threshold finding using ostut segemtation
+  level=graythresh(I2); %Threshold finding using ostut segemtation
   BW=im2bw(I2,level);
   bw1=bwareaopen(BW,800);
   figure,imshow(bw1),title('Black and White C-Cells');
